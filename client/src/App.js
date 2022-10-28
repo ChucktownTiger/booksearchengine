6483ea2df346965cb304f71f6699bc3692d09a36
ChucktownTiger <clemsonalum07@gmail.com>
2022-10-28 17:08:28 -0400

    worked on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {ApolloClient, ApolloProvider, InMemoryCache, createHttpLink} from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import {setContext} from '@apollo/client/link/context'
+
+const authLink = setContext((_, { headers }) => {
+  // this gets the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+ 
 
 function App() {
   return (
