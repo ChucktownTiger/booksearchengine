dbf87f8a5b048e83af9fe9f2a7ceb2b9a8c6b717
ChucktownTiger <clemsonalum07@gmail.com>
2022-10-28 17:13:24 -0400

    created mutations and queries

--- client/src/App.js
+++ client/src/App.js
@@ -30,8 +30,9 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <Router>
-      <>
+    <ApolloProvider  client ={client}>
+        <Router>
+        <>
         <Navbar />
         <Switch>
           <Route exact path='/' component={SearchBooks} />
@@ -39,7 +40,8 @@ function App() {
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
       </>
-    </Router>
+      </Router>
+    </ApolloProvider>
   );
 }
 
